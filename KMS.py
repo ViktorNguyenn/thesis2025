d21ed870f6a892d8fb516ca8629799f455300dc5
Viktor Nguyen <code.quangnv@gmail.com>
2026-04-23 09:19:36 +0700

    commit

---
+++ KMS.py
@@ -0,0 +1,17 @@
+"""
+Cho mảng nums và số target, tìm một cặp số trong mảng có tổng bằng target.
+
+Nếu có nhiều cặp — chỉ cần in một cặp.
+
+Nếu không có cặp nào — in ra thông báo phù hợp
+
+[1, 5, 9, 3, 12], sum = 8
+
+=> 5,3
+"""
+""" 
+Brute force 
+    - loop through all element
+    - sum each pair
+"""
+
---
+++ interview.py
@@ -0,0 +1,15 @@
+"""
+Problem 5: Palindrome Checker
+Write a program that determines whether a given string or number is a palindrome — meaning it reads the same forwards and backwards.
+Requirements:
+The input can be either a string or a number.
+Ignore spaces, punctuation, and letter case when checking for palindromes.
+Return true if the input is a palindrome, and false otherwise.
+Examples:
+Input: "madam" → Output: true
+Input: "RaceCar" → Output: true
+Input: "hello" → Output: false
+Input: 121 → Output: true
+Input: 123 → Output: false
+"""
+""""""
